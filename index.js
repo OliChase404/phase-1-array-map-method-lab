96bc6f49d420c5364806878e7727e773bd72ae4f
Oliver <olichase91@gmail.com>
2023-02-25 15:24:37 -0800

    Solved Lab

--- index.js
+++ index.js
@@ -11,6 +11,47 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+//---------------------------------------------------------------
+function titleCased(){
+const titleCased = tutorials.map((i) => {
+  let strToArr = i.split(' ').map((y) => {
+    return y.charAt(0).toUpperCase() + y.slice(1)
+  })
+  return strToArr.join(' ')
+})
+return titleCased
 }
+
+console.log(titleCased())
+//------------------------------------------------------------
+
+
+
+
+// console.log(tutorials[0].split(' '))
+
+const upperCaseTutorials = tutorials.map((i) => i.toUpperCase())
+
+// console.log(upperCaseTutorials)
+
+const reverseTutorials = tutorials.map((i) => i.split('').reverse().join(''))
+
+// console.log(reverseTutorials)
+
+
+const robots = [
+  { name: "Johnny 5", modes: 5, isActivated: false },
+  { name: "C3PO", modes: 3, isActivated: false },
+  { name: "Sonny", modes: 2.5, isActivated: false },
+  { name: "Baymax", modes: 1.5, isActivated: false },
+];
+
+const activatedRobots = robots.map((robot) => {
+  return Object.assign({}, robot, {
+    modes: robot.modes * 2,
+    isActivated: true,
+  });
+});
+
+// console.log(activatedRobots);
